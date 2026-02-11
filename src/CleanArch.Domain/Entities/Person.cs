e059f6b276a68a067568586a141c342ba5da0efd
Elkhan Isaev <elkhan.isaev@gmail.com>
2026-02-12 00:37:03 +0500

    Заменил name на lastName

--- src/CleanArch.Domain/Entities/Person.cs
+++ src/CleanArch.Domain/Entities/Person.cs
@@ -8,20 +8,20 @@ public class Person
     /// <summary>
     /// Конструктор для создания экземпляра объекта Person
     /// </summary>
-    /// <param name="name"></param>
+    /// <param name="lastName"></param>
     /// <param name="surname"></param>
     /// <param name="email"></param>
     /// <param name="phoneNumber"></param>
-    public Person(string name, string surname, string email, string phoneNumber)
+    public Person(string lastName, string surname, string email, string phoneNumber)
     {
-        Name = name;
+        LastName = lastName;
         Surname = surname;
         Email = email;
         PhoneNumber = phoneNumber;
     }
 
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string LastName { get; set; }
     public string Surname { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
